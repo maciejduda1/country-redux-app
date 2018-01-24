afa63c56b63d23c507dfcebedf60d88ecc95cb9c
Maciej Duda <maciej.duda1@gazeta.pl>
2018-01-24 13:00:06 +0100

    stage 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
-import { getCountries } from './actions/actions-countries';
+import routes from './routes';
 
 render(
     <Provider store={store}>
-        <h1>Inicjalizacja projektu</h1>
+        <Router history={hashHistory} routes={routes}/>
     </Provider>,
     document.getElementById('root')
 );
-store.dispatch(getCountries());
