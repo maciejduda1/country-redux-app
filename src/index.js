3ca32b1292dca81548bf521fcb2062049b90f501
Maciej Duda <maciej.duda1@gazeta.pl>
2018-01-24 17:04:45 +0100

    stage 3

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import routes from './routes';
-
+import {styles} from './country.css'
 render(
     <Provider store={store}>
         <Router history={hashHistory} routes={routes}/>
--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
-//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css';
 //import 'bootstrap/dist/css/bootstrap-theme.css';
 
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
             <div className="container-fluid">
                 <div className="navbar-header">
                     <Link className="navbar-brand" to="/">Państwa.js</Link>
                 </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries">Countries</Link></li>
-                        <li><Link to="/continents">Continents</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item"><Link className="nav-link" to="/countries">Countries</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/continents">Continents</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
                     </ul>
                 </div>
             </div>
